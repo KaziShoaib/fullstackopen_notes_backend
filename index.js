783f882e57eb5f3af82f0018d9d297ddc654af54
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-18 20:23:39 +0600

    added MongoDB to the backend

--- index.js
+++ index.js
@@ -1,101 +1,114 @@
-const { response } = require('express');
-const express = require('express');
+const { response, request } = require('express');
+const express = require('express');   //for express
 const app = express()
-const cors = require('cors')
 
+const cors = require('cors') // for connecting with the frontend
 app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
+
+app.use(express.json())  // for parsing json data from request.body
+app.use(express.static('build'))   // connecting the production build of front end
+
+require('dotenv').config() // accessing environment variables from the .env file
+
+// request logger middleware
+const requestLogger = (request, response, next) =>{
+  console.log("Method: ",request.method);
+  console.log("Path: ",request.path);
+  console.log("Body: ",request.body);
+  console.log('---');
+  next();
+}
+app.use(requestLogger);
 
 
-// const requestLogger = (request, response, next) =>{
-//   console.log("Method: ",request.method);
-//   console.log("Path: ",request.path);
-//   console.log("Body: ",request.body);
-//   console.log('---');
-//   next();
-// }
+const Note = require('./models/note');  //importing the Note model from the models directory
 
-// app.use(requestLogger);
-
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({error : 'unknown endpoint'})
-// }
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocollo",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
-// app.get('/', (request, response)=>{
-//   response.send('<h1>hello world</h1>');
-// })
 
 app.get('/api/notes', (request, response)=>{
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  })
 })
 
-app.get('/api/notes/:id', (request, response)=>{
-  let id = Number(request.params.id);
-  let note = notes.find(note => note.id === id);
-  if(note){
-    response.json(note);
-  }
-  else{
-    response.status(404).end();
-  }
+
+app.get('/api/notes/:id', (request, response, next)=>{
+  Note.findById(request.params.id)
+    .then(note=>{
+      if(note){
+        response.json(note);
+      }
+      else{        
+        response.status(404).end();
+      }
+    })
+    .catch(error=> next(error))
+}) 
+
+
+app.delete('/api/notes/:id', (request, response, next)=>{
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response)=>{
-  let id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-  response.status(204).end();
-})
-
-const genID = () => {
-  let maxID = notes.length > 0 ? Math.max(...notes.map(n=>n.id)) : 0;
-  return maxID + 1;
-}
 
 app.post('/api/notes', (request, response)=>{
-  let body = request.body;
-
-  if(!body.content){    
-    return response.status(400).json({
-      'error':'content missing'
-    });
-  }
-
-  let note = {
-    content: body.content,
-    id: genID(),
-    important : body.important || false,
-    date : new Date()
-  }
   
-  notes = notes.concat(note);
-  response.json(note);
+  const body = request.body
+  if(body.content === undefined){
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  let note = new Note({
+    content : body.content,
+    date : new Date(),
+    important : body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-// app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  // the edited object is not a Note model, it is a normal javascript object
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new : true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error)) //sending error the errorHandler middleware
+})
+
+
+// unknonw endpoint middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error : 'unknown endpoint'})
+}
+app.use(unknownEndpoint);
+
+
+//error Handler middleware
+const erorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if(error.name === 'CastError'){    
+    return response.status(400).send({error : 'malformatted id'});
+  }
+  next(error);
+}
+app.use(erorHandler);
+
+
+const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`);
 })
