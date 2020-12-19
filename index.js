4b6cbb14f964cf2d37e8f5079208245be8836c7d
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-19 11:43:45 +0600

    added validation

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const Note = require('./models/note');  //importing the Note model from the mode
 
 
 app.get('/api/notes', (request, response)=>{
-  Note.find({}).then(notes => {
-    response.json(notes);
-  })
+  Note.find({})
+    .then(returnedNotes => returnedNotes.map(note => note.toJSON()))
+    .then(returnedAndFormattedNotes => response.json(returnedAndFormattedNotes))
 })
 
 
@@ -35,7 +35,7 @@ app.get('/api/notes/:id', (request, response, next)=>{
   Note.findById(request.params.id)
     .then(note=>{
       if(note){
-        response.json(note);
+        response.json(note.toJSON());
       }
       else{        
         response.status(404).end();
@@ -47,14 +47,12 @@ app.get('/api/notes/:id', (request, response, next)=>{
 
 app.delete('/api/notes/:id', (request, response, next)=>{
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end();
-    })
+    .then(result => response.status(204).end())
     .catch(error => next(error))
 })
 
 
-app.post('/api/notes', (request, response)=>{
+app.post('/api/notes', (request, response, next)=>{
   
   const body = request.body
   if(body.content === undefined){
@@ -67,9 +65,10 @@ app.post('/api/notes', (request, response)=>{
     important : body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => response.json(savedAndFormattedNote))  
+    .catch(error => next(error))
 })
 
 
@@ -82,10 +81,9 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, {new : true})
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
+  Note.findByIdAndUpdate(request.params.id, note, {new : true, runValidators: true})
+    .then(updatedNote => updatedNote.toJSON())
+    .then(updatedAndFormattedNote => response.json(updatedAndFormattedNote)) 
     .catch(error => next(error)) //sending error the errorHandler middleware
 })
 
@@ -102,6 +100,9 @@ const erorHandler = (error, request, response, next) => {
   console.log(error.message);
   if(error.name === 'CastError'){    
     return response.status(400).send({error : 'malformatted id'});
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   }
   next(error);
 }
