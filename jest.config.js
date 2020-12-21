33f18a4015ea52d49138e5fe8a8b1e7ff814897d
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-21 15:45:03 +0600

    test practice with jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment:'node'
+};
+
+/* alternatively we can also write
+{
+ //...
+ "jest": {
+   "testEnvironment": "node"
+ }
+}
+at the end of the package.json file
+the we won't have to create this seperate jest.config.js file
+*/
---
+++ test/palindrome.test.js
@@ -0,0 +1,25 @@
+//jest test files should have .test in their name
+
+const palindrome = require('../utils/for_testing').palindrome;
+
+describe('palindrome', () => {
+
+  test('palindrome of a', () => {
+    const result = palindrome('a');
+    expect(result).toBe('a');
+  });
+
+  test('palidrome of react', () => {
+    const result = palindrome('react');
+    expect(result).toBe('tcaer');
+  });
+
+  test('palidrmoe of releveler', () => {
+    const result = palindrome('releveler');
+    expect(result).toBe('releveler');
+  });
+
+});
+
+
+
